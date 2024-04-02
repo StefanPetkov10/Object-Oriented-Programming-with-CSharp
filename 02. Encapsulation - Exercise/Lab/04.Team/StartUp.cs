3a478c7c8bb4c1e716022130c9ee50fea6d2b959
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-04-02 13:41:42 +0300

    Add Encapsulation task - Lab

---
+++ 02. Encapsulation - Exercise/Lab/04.Team/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Team team = new Team("SoftUni");
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputInfo = Console.ReadLine().Split();
+
+                int age = int.Parse(inputInfo[2]);
+                decimal salary = decimal.Parse(inputInfo[3]);
+
+                Person person = new Person(inputInfo[0], inputInfo[1], age, salary);
+
+                team.AddPlayer(person);
+            }
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players");
+            Console.WriteLine($"Reserves team has {team.ReserveTeam.Count} players");
+        }
+    }
+}
