0203f54f2d3cc5f32fb076e45c5039fc53518701
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-04-04 22:50:37 +0300

    Add Encapsulation task - Exercise

---
+++ 02. Encapsulation - Exercise/03.ShoppingSpree/Models/ExceptionMessages.cs
@@ -0,0 +1,15 @@
+﻿namespace ShoppingSpree.Models
+{
+    public class ExceptionMessages
+    {
+        public static void NameEmpty()
+        {
+            throw new ArgumentException("Name cannot be empty");
+        }
+
+        public static void MoneyNegative()
+        {
+            throw new ArgumentException("Money cannot be negative");
+        }
+    }
+}
