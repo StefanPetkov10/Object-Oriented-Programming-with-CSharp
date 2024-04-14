d7bed713d120c36f5eaa12fb6ebf2755fa462853
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-04-14 22:55:20 +0300

    Add Interfaces And Abstraction - Exercise

---
+++ 03. Interfaces and Abstraction - Exercise/07.MilitaryElite/Models/Repair.cs
@@ -0,0 +1,27 @@
+﻿using _07.MilitaryElite.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.MilitaryElite.Models
+{
+    public class Repair : IRepair
+    {
+        public Repair(string partName, int hoursWorked)
+        {
+            PartName = partName;
+            HoursWorked = hoursWorked;
+        }
+
+        public string PartName { get; private set; }
+
+        public int HoursWorked { get; private set; }
+
+        public override string ToString()
+        {
+            return $"Part Name: {PartName} Hours Worked: {HoursWorked}";
+        }
+    }
+}
