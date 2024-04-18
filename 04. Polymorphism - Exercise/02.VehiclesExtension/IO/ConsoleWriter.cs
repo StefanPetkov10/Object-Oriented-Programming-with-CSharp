6bc04091b957bfe9ef929bdf2b7ff2b6a5fa07fe
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-04-18 15:18:26 +0300

    Add Polymorphism - Exercise

---
+++ 04. Polymorphism - Exercise/02.VehiclesExtension/IO/ConsoleWriter.cs
@@ -0,0 +1,8 @@
+﻿using VehiclesExtension.IO.Interfaces;
+
+namespace VehiclesExtension.IO;
+
+public class ConsoleWriter : IWriter
+{
+    public void WriteLine(string str) => Console.WriteLine(str);
+}
