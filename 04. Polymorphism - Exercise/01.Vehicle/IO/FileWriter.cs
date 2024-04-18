03cad553dc7dcb950aabfd3cf87bd06e18558a8d
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-04-18 15:17:42 +0300

    Add Polymorphism - Exercise

---
+++ 04. Polymorphism - Exercise/01.Vehicle/IO/FileWriter.cs
@@ -0,0 +1,13 @@
+﻿using Vehicles.IO.Interfaces;
+
+namespace Vehicles.IO;
+
+public class FileWriter : IWriter
+{
+    public void WriteLine(string str)
+    {
+        using StreamWriter writer = new("D:\\test.txt", true);
+
+        writer.WriteLine(str);
+    }
+}
