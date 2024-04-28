80f982fc057fabba898e1dada4aac35b6a1f3ec3
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-04-28 23:12:37 +0300

    Delete

--- 07. Reflection and Attributes/02.ValidationAttributes/Attributes/MyRequiredAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ValidationAttributes.Attributes;
-
-public class MyRequiredAttribute : MyValidationAttribute
-{
-    public override bool IsValid(object obj)
-        => obj is not null;
-}
